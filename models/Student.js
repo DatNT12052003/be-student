ad6711d5292f25024d40cb9c7086d9cb64d26c44
DatNT12052003 <thanhdat2003pt@gmail.com>
2024-11-29 09:06:59 +0700

    add mongodb

--- models/Student.js
+++ models/Student.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const queryString = process.env.MONGODB_URI || "mongodb+srv://thanhdat2003pt:4Nhs3ch03m@cluster0.i8yo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+//configure mongoose
+mongoose.connect(queryString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected!'))
+.catch(err => console.log('MongoDB connection error:', err.message));
+
+
 const StudentSchema = new Schema({
     studentId: Number,
     name: String,
